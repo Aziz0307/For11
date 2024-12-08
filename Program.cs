116d04bf1ee43cb9ca840a163ff77614eabd9ea7
Aziz0307 <Aziz03072018@gmail.com>
2024-12-08 12:29:30 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace For11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int Sum = 0;
+            for (int i = n; i <=2 * n; i++)
+            {
+                Sum = Sum + (i * i);
+            }
+            Console.WriteLine(Sum);
+        }
+    }
+}
